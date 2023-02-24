57072a7ed2cafb4a98cfebef8dbe1862dc5e7c11
Nolen Lawton <mnlawton@icloud.com>
2023-02-23 22:28:59 -0600

    created store

--- src/index.js
+++ src/index.js
@@ -3,9 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+
+const reducer = (state = [], action) => {
+    return action.payload
+}
+
+const store = createStore(
+    combineReducers({
+        reducer,
+    }),
+    applyMiddleware(logger)
+)
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
